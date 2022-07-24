62fab5769a1d876bfd6f516de2b4d0dc3445e4e6
Philipp Mendogralo <abkxj59@gmail.com>
2022-07-25 01:18:07 +0500

    Добавляет хедер, футер, промо и путешествия

--- source/js/main.js
+++ source/js/main.js
@@ -1,46 +1,24 @@
-import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
+const nav = document.querySelector('.nav');
+const toggleButton = document.querySelector('.nav__toggle-button');
+const toggleImg = document.querySelector('.nav__toggle-img');
 
-// ---------------------------------
+const initMenu = () => {
+  nav.classList.remove('nav--nojs');
+  nav.classList.toggle('nav--opened');
+  nav.classList.toggle('nav--closed');
+  // toggleImg.src = 'img/svg/menu-open.svg';
+  toggleButton.hidden = false;
 
-window.addEventListener('DOMContentLoaded', () => {
-
-  // Utils
-  // ---------------------------------
-
-  iosVhFix();
-
-  // Modules
-  // ---------------------------------
-
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
-  window.addEventListener('load', () => {
-    initModals();
+  toggleButton.addEventListener('click', () => {
+    nav.classList.toggle('nav--opened');
+    nav.classList.toggle('nav--closed');
+    if (nav.classList.contains('nav--opened')) {
+      toggleImg.src = 'img/svg/menu-close.svg';
+      // toggleImg.width = '13px';
+    } else {
+      toggleImg.src = 'img/svg/menu-open.svg';
+    }
   });
-});
+};
 
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используейтся matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
+initMenu();
